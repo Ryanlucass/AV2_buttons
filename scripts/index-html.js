56d29fc31af198ee36e6a44c5e803a767e678d85
Lucas alves <lucasryanalves@gmail.com>
2020-11-27 22:23:11 -0300

    Copiei o código do meu outro arquivo
    Copiei o código para tentar postar em uma url

---
+++ scripts/index-html.js
@@ -0,0 +1,11 @@
+function teste01(){
+    window.alert('você clicou no botão 01')
+}
+
+function teste02(){
+    window.alert('você clicou no botão 02')
+}
+
+function teste03(){
+    window.alert('você clicou no botão 03')
+}
